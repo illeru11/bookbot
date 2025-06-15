650eba46e926dab12b82a70bdb8d5f4b0d81d2bf
illeru11 <jlwillson@gmail.com>
2025-06-15 14:29:17 +1000

    Bookbot character counter  working

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+from stats import count_words, count_char
+
 def get_book(path_to_file):
     #Pulls in a string from a file
     with open(path_to_file) as f:
         file_string = f.read()
-        #print(f"Text length: {len(file_string)} characters")
-        #print(f"First 100 characters: {file_string[:100]}")
-        
-        return file_string
-def count_words(text):
-    #Counts the number of words in a string
+    return file_string
+
+
+def count_char(text):
+    #Counts the number of characters in a string
     if text is None:
         return 0
-    words = text.split()
-    return len(words)
+    small_text = text.lower()
+    small = list(small_text)
+
+    #alphabet_string = "abcdefghijklmnopqrstuvwxyz"
+    #alphabet = list(alphabet_string)
+    
+    count_list = {}
+    alphabet = []
+    #set values of the first dictionary
+    #for char in range(0,len(alphabet)+1):
+        #count_list ={char: 0}
+    
+    for s in small:
+        if s in count_list:
+            count_list[s] += 1   
+        else:
+            count_list[s] = 1 
+
+    return count_list
+
+    
+
+    
+    #text.split
+    
+    #return len(text)
 
 def main():
     book = "books/frankenstein.txt"
@@ -20,4 +45,13 @@ def main():
     w_count = count_words(text)
     #expected_result = 75767
     print(f"{w_count} words found in the document")
-main()
+    c_count =count_char(text)
+    print(f"{c_count}")
+
+
+
+#end
+
+    #value = count_char(text)
+    #print (value)
+main()  
